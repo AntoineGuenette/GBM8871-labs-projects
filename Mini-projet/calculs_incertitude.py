c39dc6528a6d1e3b8640cc6de127e0e8b95b0e32
AntoineGuenette <anto.guenette@gmail.com>
2025-10-10 10:02:34 -0400

    Add speed computation

--- Mini-projet/calculs_incertitude.py
+++ Mini-projet/calculs_incertitude.py
@@ -15,6 +15,9 @@ delta_v = 0.04e-2 # m/s
 
 r = 6.515e-4 # m
 
+w_4 = 2e-3 # m
+h = 250e-6 #m
+
 #######################################
 # CALCUL DU DÉBIT ET DE SON INCERTITUDE
 #######################################
@@ -24,3 +27,10 @@ print(f"Débit: {Q:.3e} m3/s")
 
 delta_Q = np.sqrt((v/L * delta_V)**2 + (V*v/(L**2) * delta_L)**2 + (V/L * delta_v)**2)
 print(f"Incertitude sur le débit: {delta_Q:.3e} m3/s")
+
+######################################
+# CALCUL DE LA VITESSE DANS LE ROUTEUR
+######################################
+
+v_routeur = Q / (w_4 * h)
+print(f"Vitesse: {v_routeur:.4e} m/s")
