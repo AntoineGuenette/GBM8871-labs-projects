f0409ac266691d0c5701ee14f880fb2b5ae6e9d9
AntoineGuenette <anto.guenette@gmail.com>
2025-10-29 21:39:37 -0400

    Use log scale for y axis in Question 1e

--- Devoir_2/Question_1.py
+++ Devoir_2/Question_1.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# C)
+###################################
+# DONNÉES PHYSIQUES ET GÉOMÉTRIQUES
+###################################
 
 # Définitions des constantes
 kB = 1.380649e-23 # (J/K)
@@ -11,31 +13,40 @@ rho_eau = 1000 # (kg/m3)
 g = 9.81 # (m/s2)
 H = 0.10 # (m)
 
-# Générations des diamètres des particules
+#################
+# SOUS-QUESTION C
+#################
+
+# Génération des diamètres des particules
 d = np.logspace(-9, -6, 200) # m
 
 # Calcul du nombre de Péclet
 Pe = (np.pi * d**3 * (rho_or - rho_eau) * g * H) / (6 * kB * T)
 
-# E)
-# Calcul du h_50
-h50 = -(H / Pe) * np.log(0.5 * (1 + np.exp(-Pe)))
-
-plt.figure(figsize=(10, 6))
-# Plot C)
-plt.subplot(2, 1, 1)
+# Figure pour la sous-question C
+plt.figure(figsize=(8, 5))
 plt.plot(d * 1e9, Pe)
 plt.xlabel("Diamètre, d, des particules d'or (nm)")
 plt.ylabel("Nombre de Péclet (Pe)")
 plt.title("Nombre de Péclet en fonction du diamètre des nanoparticules d'or")
 plt.grid(True)
+plt.tight_layout()
+plt.show()
 
-#Plot E)
-plt.subplot(2, 1, 2)
+#################
+# SOUS-QUESTION E
+#################
+
+# Calcul du h_50
+h50 = -(H / Pe) * np.log(0.5 * (1 + np.exp(-Pe)))
+
+# Figure pour la sous-question E
+plt.figure(figsize=(8, 5))
 plt.plot(d * 1e9, h50 * 100)
+plt.yscale("log")
 plt.xlabel("Diamètre d (nm)")
 plt.ylabel("Hauteur h_50 (cm)")
 plt.title("Hauteur d’équilibre h_50 en fonction du diamètre des nanoparticules d’or")
-plt.grid(True)
+plt.grid(True, which="both")
 plt.tight_layout()
 plt.show()
