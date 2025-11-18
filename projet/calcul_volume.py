b7ed9b03b3fe1589d3cc0cfbbd28a1e049503c15
AntoineGuenette <anto.guenette@gmail.com>
2025-11-17 21:34:37 -0500

    Update

--- projet/calcul_volume.py
+++ projet/calcul_volume.py
@@ -14,7 +14,7 @@ h_puits = 630e-6
 W_canal = 700e-6
 h_canal = 700e-6
 
-dist_x = 3e-3
+dist_x = 2e-3
 dist_y = 6e-3
 
 # Calculs associés
@@ -37,7 +37,7 @@ A_outlet = np.pi * r_outlet**2
 V_puits = L_puits * W_puits * h_puits
 V_raman = A_raman * h_puce
 V_inlet = A_inlet * h_canal
-V_outlet = A_outlet * h_canal
+V_outlet = A_outlet * (h_canal + h_puce)
 print(f"\nVolume d'un puits : {V_puits*1e9:.2f} uL")
 print(f"Volume de raman : {V_raman*1e9:.2f} uL")
 print(f"Volume d'entrée : {V_inlet*1e9:.2f} uL")
@@ -45,7 +45,7 @@ print(f"Volume de sortie : {V_outlet*1e9:.2f} uL")
 
 V_canal = W_canal * h_canal * (dist_tot - r_inlet - r_outlet)
 
-V_tot = 3 * V_puits + V_inlet + V_outlet + V_canal # + 3 * V_raman
+V_tot = 3 * V_puits + V_inlet + V_outlet + V_canal + 3 * V_raman
 print(f"\nVolume total : {V_tot*1e9:.2f} uL")
 print(f"Première approximation de V_max : {V_tot*1.2*1e9:.2f} uL")
 
